a6ac4b0d58231c924d3281331dd7e69df83947b1
daniel.jaramillo24 <daniel.jaramillo24@udea.edu.co>
2026-09-20 18:10:05 -0500

    feat: conectar las rutas de la aplicación al layout y a la sesión

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
 import HU01RegistroDeCliente from './pages/HU01RegistroDeCliente';
-import HU12SolicitarCita from './pages/HU12SolicitarCita';
-import HU09DefinirHorariosDisponibles from './pages/HU09DefinirHorariosDisponibles';
-import HU06RegistrarEspecialidad from './pages/HU06RegistrarEspecialidad';
 import HU02InicioDeSesion from './pages/HU02InicioDeSesion';
-function App() {
+import HU06RegistrarEspecialidad from './pages/HU06RegistrarEspecialidad';
+import HU08ConsultarEspecialidades from './pages/HU08ConsultarEspecialidades';
+import HU09DefinirHorariosDisponibles from './pages/HU09DefinirHorariosDisponibles';
+import HU12SolicitarCita from './pages/HU12SolicitarCita';
+import NoEncontrada from './pages/comunes/NoEncontrada';
+import Inicio from './routes/Inicio';
+import { RUTAS } from './routes/paths';
+
+export default function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
         <Routes>
-			<Route path="/" element={<HU01RegistroDeCliente />} />
-			<Route path="/HU01RegistroDeCliente" element={<HU01RegistroDeCliente />} />
-			<Route path="/solicitar-cita" element={<HU12SolicitarCita />} />
-			<Route path="/horarios" element={<HU09DefinirHorariosDisponibles />} />
-			<Route path="/registrar-especialidad" element={<HU06RegistrarEspecialidad />} />
-			<Route path="/login" element={<HU02InicioDeSesion />} />
+          <Route element={<Layout />}>
+            <Route path={RUTAS.inicio} element={<Inicio />} />
+            <Route path={RUTAS.registro} element={<HU01RegistroDeCliente />} />
+            <Route path={RUTAS.login} element={<HU02InicioDeSesion />} />
+            <Route path={RUTAS.especialidades} element={<HU08ConsultarEspecialidades />} />
+            <Route
+              path={RUTAS.registrarEspecialidad}
+              element={
+                <ProtectedRoute roles={['ROLE_ESPECIALISTA']}>
+                  <HU06RegistrarEspecialidad />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<NoEncontrada />} />
+          </Route>
+
+          {/* Pantallas que aún no usan <Layout> (HU-09 y HU-12): se migran en su propia HU */}
+          <Route path={RUTAS.horarios} element={<HU09DefinirHorariosDisponibles />} />
+          <Route path={RUTAS.solicitarCita} element={<HU12SolicitarCita />} />
+
+          {/* Enlace anterior del prototipo */}
+          <Route path="/HU01RegistroDeCliente" element={<Navigate to={RUTAS.registro} replace />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
-export default App;
